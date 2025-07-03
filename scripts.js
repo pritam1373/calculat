6836cbe36ffd98909a2d2c1c3665830146fb9b87
pritam1373 <ovnandy99@gmail.com>
2025-07-03 08:32:40 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,10 @@
+a=document.getElementById("one")
+b=document.getElementById("two")
+c=document.getElementById("next")
+
+a.addEventListener("click",()=>{
+    c.addEventListener("click",() =>{
+        window.location.href="index2.html"
+    })
+})
+
